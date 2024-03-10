a3e635b1f4a7ce85e37c9d2b92547d2173a1c5b4
SANJAY KUMAR D <92215315+SanjayKumarDD@users.noreply.github.com>
2024-03-10 16:15:59 +0530

    meta data form

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-document.getElementById("image-input").addEventListener("change", function () {
-  var file = this.files[0];
-  if (file) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      var imagePreview = document.getElementById("image-preview");
-      imagePreview.innerHTML = ""; // Clear previous preview
-      var img = new Image();
-      img.src = e.target.result;
-      img.style.maxWidth = "100%";
-      img.style.maxHeight = "100%";
-      imagePreview.appendChild(img);
+// uploading the image and displaying it
+let cellid;
+function uploadImage() {
+  var input = document.getElementById("imageInput");
+  var file = input.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (event) {
+    var img = new Image();
+    img.src = event.target.result;
+    img.onload = function () {
+      var container = document.getElementById("imageContainer");
+      var idElement = document.getElementById("imageId");
+      var elementId = Math.floor(Math.random() * 9000000000) + 1000000000;
+      img.id = elementId;
+      cellid = elementId;
+      container.style.display = "block";
+      container.innerHTML = "";
+      container.appendChild(img);
+      idElement.innerHTML = "Cell ID: " + elementId;
+      let text = cellid;
+      JsBarcode("#barcode", text);
+      // Display content div after image upload
+      document.getElementById("contentDiv").style.display = "block";
     };
-    reader.readAsDataURL(file);
-  }
-});
+  };
+
+  reader.readAsDataURL(file);
+}
+
+// generating bar code
+
+// document.getElementById("butUpload").addEventListener("click", () => {
+//   // let text = document.getElementById("text").value;
+//     let text = cellid;
+//   JsBarcode("#barcode", text);
+// });
